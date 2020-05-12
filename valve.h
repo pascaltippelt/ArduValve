18aeec1a4e3d0d6b540641fd1f45ff7096598236
Pascal Tippelt <mail@pascaltippelt.de>
2020-05-12 12:08:15 +0200

    Initial Commit

---
+++ valve.h
@@ -0,0 +1,39 @@
+#ifndef Morse_h
+#define Morse_h
+
+#include "Arduino.h"
+
+#define OPEN LOW
+#define CLOSE HIGH
+
+#define AUTOMATIC true
+#define MANUAL false
+
+struct valveTimer {
+  int min_on = 360;
+  int min_off = 420;  
+};
+
+class Valve
+{
+	public:
+		Valve(int pin);
+		
+		bool getState();
+		void setState(bool state);
+		
+		bool getMode();
+		void setMode(bool mode);
+	
+		valveTimer timer;
+	
+	private:
+		int _pin;		//wich pin is the valve attached to
+		bool _state	= CLOSE;	//wich state has the pin / valve
+		bool _mode = MANUAL;
+	
+};
+
+
+
+#endif
